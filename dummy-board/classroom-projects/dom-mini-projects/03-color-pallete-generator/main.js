5f55312fa20a96b74b877c215cb61f9fc1066c51
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-03-13 00:17:51 +0530

    Add color pallete generator code

--- dummy-board/classroom-projects/dom-mini-projects/03-color-pallete-generator/main.js
+++ dummy-board/classroom-projects/dom-mini-projects/03-color-pallete-generator/main.js
@@ -1,32 +1,62 @@
-const input = document.getElementById("itemInput");
-const addBtn = document.getElementById("addBtn")
+const formatSelect = document.getElementById("format");
+const toneSelect = document.getElementById("tone");
+const button = document.getElementById("generateBtn");
 
-const list = document.getElementById("list")
-console.log(list);
+function randomRGB(tone) {
+  let min = 0;
+  let max = 255;
 
+  if (tone === "light") {
+    min = 150;
+    max = 255;
+  }
 
-addBtn.addEventListener("click" , ()=>{
-    if(input.value === ""){
-      alert("Mat Kar Lala")
-      return
+  if (tone === "dark") {
+    min = 0;
+    max = 125;
+  }
+
+  const r = Math.floor(Math.random() * (max - min) + min);
+  const g = Math.floor(Math.random() * (max - min) + min);
+  const b = Math.floor(Math.random() * (max - min) + min);
+
+  return { r, g, b };
+}
+
+function rgbToHex(r, g, b) {
+  // (0,0,0) - black
+  // (255,0,0) - red
+  // (0,255,0) - green
+  // (0,0,255) - blue
+  // (255,255,255) - white
+
+  return "#" + [r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("");
+}
+
+function generatePalette() {
+  palette.innerHTML = "";
+
+  for (let i = 0; i < 5; i++) {
+    const { r, g, b } = randomRGB(toneSelect.value);
+
+    let color;
+
+    if (formatSelect.value === "hex") {
+      color = rgbToHex(r, g, b);
+    } else {
+      color = `rgb(${r}, ${g}, ${b})`;
     }
 
+    const div = document.createElement("div");
+    div.classList.add("color");
 
-    // list.innerHTML += `<li>${input.value}</li>`
-    const li = document.createElement("li")
-    const delBtn = document.createElement("button")    
-    delBtn.textContent = "Delete"
-    delBtn.classList.add("delete")
+    div.style.background = `rgb(${r}, ${g}, ${b})`;
+    div.textContent = color;
 
-    li.textContent = input.value
+    palette.appendChild(div);
+  }
+}
 
-    delBtn.addEventListener("click", () => {
-      list.removeChild(li)
-    })
+button.addEventListener("click", generatePalette);
 
-    li.appendChild(delBtn)
-    list.appendChild(li)
-
-    // Make this editable and autoSave when hatana
-    input.value = ""
-})
+generatePalette();
