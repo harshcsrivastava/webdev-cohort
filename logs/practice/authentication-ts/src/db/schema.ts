aa75350a5b9bee9192e6e2a85b68cab25933107e
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-04-01 23:27:24 +0530

    feat: add drizzle ORM setup and user table schema
    - Updated package.json to include drizzle-orm, drizzle-kit, and pg dependencies.
    - Created drizzle.config.js for drizzle-kit configuration.
    - Added SQL migration file for creating users table with necessary fields.
    - Added metadata files for drizzle migrations and snapshots.
    - Implemented database connection in src/db/index.ts.
    - Defined user table schema in src/db/schema.ts using drizzle-orm.

---
+++ logs/practice/authentication-ts/src/db/schema.ts
@@ -0,0 +1,18 @@
+import { uuid, pgTable, varchar, boolean, text, timestamp } from "drizzle-orm/pg-core";
+
+// user: id, firstName, lastName, email, password, salt, role, isEmailVerified
+export const userTable = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+
+    firstName: varchar("first_name", { length: 45 }).notNull(),
+    lastName: varchar("last_name", { length: 45 }),
+
+    email: varchar("email", { length: 322 }).notNull().unique(),
+    emailVerified: boolean("email_Verified").default(false).notNull(),
+
+    password: varchar("password", { length: 66 }),
+    salt: text("salt"),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
