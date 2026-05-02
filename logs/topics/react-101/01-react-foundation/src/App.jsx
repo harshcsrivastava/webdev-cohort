5880beccceba285bcde8e39bf6d224a432a8aaec
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-05-02 23:20:37 +0530

    Learn about useEffect and Abort Controller

--- logs/topics/react-101/01-react-foundation/src/App.jsx
+++ logs/topics/react-101/01-react-foundation/src/App.jsx
@@ -1,22 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-    // when function is given in useState default param it is called a LAZY-STATE Initialisation
-    // count => value, setCount => funct
+    const [posts, setPosts] = useState([]);
+    const [status, setStatus] = useState("idle");
+    const [seconds, setSeconds] = useState(10);
 
-    // react ki bhasha functions = hook
-    // 2 popular - useState & useEffect
+    // useEffect(() => {
 
-    const increase = () => {
-        setCount(count + 1);
-        console.log(count);
-    };
+    //     return () => {
+    //cleanup - memory ye hold nhi rakhna, deleted the reference like in websocketd
+    // Without the cleanup, if the component unmounted or re-rendered, the interval would keep running in the background, possibly
+    // updating state on an unmounted component → leading to warnings or wasted resources.
+    //     }
+    // }, [dependency-array])
+    // It runs on component mount
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setSeconds((current) => Math.max(current - 1, 0));
+        }, 1000);
+        return () => {
+            //cleanup
+            clearInterval(timerId);
+        };
+    }, []);
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        async function loadPost() {
+            try {
+                setStatus("loading");
+                const response = await fetch(
+                    "https://jsonplaceholder.typicode.com/posts?_limit=5",
+                    { signal: controller.signal },
+                );
+                const data = await response.json();
+
+                setPosts(data);
+                setStatus("Success");
+            } catch (error) {
+                // if error aya then
+                setStatus("Error");
+            }
+        }
+
+        loadPost();
+
+        return () => {
+            // user kisi aur page pe chala gya hai, to component unmount hogya, lekin request chli gyi and data me save hogya to component ko mana krta.
+            controller.abort();
+        };
+    }, []);
+
+    // if(status){
+    //     return <LoadingComponent/>
+    // }
+
     return (
         <>
             <h1>Still Alive, Still Breathing.</h1>
-            <h1>Value = {count}</h1>
-            <button onClick={increase}>Click Me</button>
+            <h3>{seconds}</h3>
         </>
     );
 }
